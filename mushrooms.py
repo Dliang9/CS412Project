f7449ae19143db332595d2168756fecc3a75128a
Anh Tuan Tran <anhtuantran@10-8-229-138.vlan877.client.wireless.uic.edu>
2017-11-09 11:50:39 -0600

    added the calculation for prior distribution P(y=edible)

--- mushrooms.py
+++ mushrooms.py
@@ -9,11 +9,18 @@ header = []
 
 # 2d array that contains data
 data = []
+edible_count = 0
 
 for line in lines:
    if (count ==0):
       header = line[0].split( ",")
+      header.pop(0)   # the first item is result
    else:
-      data.append(line[0].split(","))
+      tmp = line[0].split(",")
+      if (tmp.pop(0) == 'e'):
+          edible_count += 1
+      data.append(tmp)
    count += 1
  
+# calcuate the prior distribution for P(y=edible)
+p_e = edible_count/count

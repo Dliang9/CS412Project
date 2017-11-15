0accd47e69beab1b1600c90db6176715cbf84424
lihengbin333 <hli217@uic.edu>
2017-11-14 19:03:33 -0600

    Update mushrooms.py
    Added matrix for negative class.
    Added codes to compute probabilities.
    Added codes to print some detail.

--- mushrooms.py
+++ mushrooms.py
@@ -2,30 +2,43 @@ import csv
 
 def featureProbability(header,data):
     #count number of each variable for each feature
-    count = {}
-    for r in range(0,len(data)):
-        if data[r][0] == "e":
-            for c in range(0,len(header)):
-                if not header[c] in count:
-                    count[header[c]] = {}
-                if not data[r][c] in count[header[c]]:
-                    count[header[c]][data[r][c]] = 0
-                count[header[c]][data[r][c]] += 1
+    count_pos = {}
+    count_neg = {}
+    
+    for r in range(0, len(header)):
+        if not header[r] in count_pos:
+            count_pos[header[r]] = {}
+        if not header[r] in count_neg:
+            count_neg[header[r]] = {}
+        for c in range(0, len(data)):
+            if not data[c][r] in count_pos[header[r]]:
+                count_pos[header[r]][data[c][r]] = 0
+            if not data[c][r] in count_neg[header[r]]:
+                count_neg[header[r]][data[c][r]] = 0
+            if data[c][0] == "e":
+                count_pos[header[r]][data[c][r]] += 1
+            else:
+                count_neg[header[r]][data[c][r]] += 1
+                    
     
     #edible probability of each variable 
     prob = {}
-    for r in count:
+    for r in count_pos:
+        print("##################################")
         print(r)
-        for c in count[r]:
-            
+        for c in count_pos[r]:
+            #print(count_pos[r])
             if not r in prob:
                 prob[r] = {}
             if not c in prob[r]:
                 prob[r][c] = 0
-            
-            #need to divide by no. of each variable not data\s length
-            prob[r][c] = count[r][c]/len(data)
+            #probability of each variable = # of edible / total # of the variable in its feature
+            prob[r][c] = count_pos[r][c]/(count_pos[r][c]+count_neg[r][c])
+            print("++++++++++++++++++++++++++++++++++")
             print(c)
+            print("e ",count_pos[r][c])
+            print("p ",count_neg[r][c])
+            print("----------------------------------")
             print(prob[r][c])
             
 

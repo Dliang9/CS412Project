d157ffefbd1ffa816a1a37d429ae356177d305a5
Anh Tuan Tran <anhtuantran@10-8-233-217.vlan877.client.wireless.uic.edu>
2017-11-16 11:53:08 -0600

    calcuated the likelihood and prector probs

--- mushrooms.py
+++ mushrooms.py
@@ -1,6 +1,6 @@
 import csv
 
-def featureProbability(header,data):
+def likelihoodProbability(header,data):
     #count number of each variable for each feature
     count_pos = {}
     count_neg = {}
@@ -24,24 +24,43 @@ def featureProbability(header,data):
     #edible probability of each variable 
     prob = {}
     for r in count_pos:
-        print("##################################")
-        print(r)
+        # print("##################################")
+        # print(r)
         for c in count_pos[r]:
-            #print(count_pos[r])
+            # print(count_pos[r])
             if not r in prob:
                 prob[r] = {}
             if not c in prob[r]:
                 prob[r][c] = 0
             #probability of each variable = # of edible / total # of the variable in its feature
             prob[r][c] = count_pos[r][c]/(count_pos[r][c]+count_neg[r][c])
-            print("++++++++++++++++++++++++++++++++++")
-            print(c)
-            print("e ",count_pos[r][c])
-            print("p ",count_neg[r][c])
-            print("----------------------------------")
-            print(prob[r][c])
+            # print("++++++++++++++++++++++++++++++++++")
+            # print(c)
+            # print("e ",count_pos[r][c])
+            # print("p ",count_neg[r][c])
+            # print("----------------------------------")
+            # print(prob[r][c])
+    return prob
             
+def calculate_predictor_prob(header, data):
+    num_features = len(header)
+    i = 0
+    predictor_prob = {}
+    while (i < num_features) :
+         predictor_prob[header[i]] = {}
+         for j in range (0, len(data)-1): 
+             if (data[j][i] in predictor_prob[header[i]]):
+                predictor_prob[header[i]][data[j][i]] += 1
+             else:
+                predictor_prob[header[i]][data[j][i]] = 0
+         i += 1
+    for feature in header:
+        for value in predictor_prob[feature]:
+            predictor_prob[feature][value] = predictor_prob[feature][value] / len(data)
+         
+    return predictor_prob;
 
+    
     
 def main():
     rd = open("mushrooms.csv")
@@ -57,20 +76,21 @@ def main():
 
     for line in lines:
         if (count ==0):
-            
             header = line[0].split( ",")
             header.pop(0)   # the first item is result
         else:
             tmp = line[0].split(",")
             if (tmp[0] == 'e'):
                 edible_count += 1
+            tmp.pop(0)
             data.append(tmp)
         count += 1
  
     # calcuate the prior distribution for P(y=edible)
     p_e = edible_count/count
     
-    featureProbability(header,data)
-    
+    prob = likelihoodProbability(header,data)
+    predictor_probs = calculate_predictor_prob(header, data)
+
 if __name__=="__main__":
     main()
